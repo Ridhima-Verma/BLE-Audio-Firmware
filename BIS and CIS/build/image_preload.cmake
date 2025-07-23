c780b755f89217affd05827d2757b5498d9d5404
Ridhima <81485504+Ridhima-Verma@users.noreply.github.com>
2025-07-23 14:38:50 +0530

    Add files via upload

---
+++ BIS and CIS/build/image_preload.cmake
@@ -0,0 +1,7 @@
+# Generated file that can be used to preload variant images
+set(BOARD "nrf5340_audio_dk_nrf5340_cpuapp" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "c:/NCSsemi/v2.6.0/nrf/applications/nrf5340_audio/prj.conf" CACHE INTERNAL "NCS child image controlled")
+set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "dfu/conf/overlay-dfu_external_flash.overlay" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "C:/NCSsemi/toolchains/cf2149caf2/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
